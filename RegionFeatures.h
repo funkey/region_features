478ae7ab61a4846cf5fde0ff19124fb8c9a4d82b
Jan Funke <funke@ini.ch>
2014-10-10 16:31:58 +0200

    implemented RegionFeatures and feature group Statistics

---
+++ RegionFeatures.h
@@ -0,0 +1,82 @@
+#ifndef REGION_FEATURES_REGION_FEATURES_H__
+#define REGION_FEATURES_REGION_FEATURES_H__
+
+#include <map>
+#include <vector>
+#include <vigra/multi_array.hxx>
+#include "Statistics.h"
+
+/**
+ * Class to compute and store selected region features for a labeled image.
+ */
+template <unsigned int N, typename ValueType, typename LabelType>
+class RegionFeatures {
+
+public:
+
+	/**
+	 * A selection of the features to compute.
+	 */
+	struct Selection {
+
+	};
+
+	/**
+	 * Compute region features on the given image. Regions are identified by 
+	 * having the same label in a separate view.
+	 *
+	 * @param image
+	 *              The raw image.
+	 *
+	 * @param labels
+	 *              A label image that identifies regions.
+	 *
+	 * @param selection
+	 *              A selection of features to compute. If not given, all 
+	 *              available features will be computed.
+	 */
+	void computeFeatures(
+			vigra::MultiArrayView<N, ValueType> image,
+			vigra::MultiArrayView<N, LabelType> labels,
+			const Selection& selection = Selection());
+
+	/**
+	 * Get the computed features for the region with the given label.
+	 *
+	 * @param region
+	 *              The label of the region.
+	 *
+	 * @return A vector of all features for this region.
+	 */
+	const std::vector<double>& getFeatures(LabelType region) { return _features[region]; }
+
+private:
+
+	// convenience typedefs
+	typedef region_features::Statistics<N, ValueType, LabelType> Statistics;
+
+	std::map<LabelType, std::vector<double> > _features;
+};
+
+////////////////////
+// IMPLEMENTATION //
+////////////////////
+
+template <unsigned int N, typename ValueType, typename LabelType>
+void
+RegionFeatures<N, ValueType, LabelType>::computeFeatures(
+		vigra::MultiArrayView<N, ValueType> image,
+		vigra::MultiArrayView<N, LabelType> labels,
+		const Selection& /*selection*/) {
+
+	_features.clear();
+
+	// if statistics are selected
+	RegionFeatures::Statistics::fill(
+			image,
+			labels,
+			_features);
+}
+
+#endif // REGION_FEATURES_REGION_FEATURES_H__
+
---
+++ Statistics.h
@@ -0,0 +1,86 @@
+#ifndef REGION_FEATURES_STATISTICS_H__
+#define REGION_FEATURES_STATISTICS_H__
+
+#include <vigra/accumulator.hxx>
+
+namespace region_features {
+
+template <unsigned int N, typename ValueType, typename LabelType>
+class Statistics {
+
+public:
+
+	static void fill(
+			const vigra::MultiArrayView<N, ValueType>& image,
+			const vigra::MultiArrayView<N, LabelType>& labels,
+			std::map<LabelType, std::vector<double> >& features) {
+
+		using namespace vigra;
+		using namespace vigra::acc;
+
+		typedef UserRangeHistogram<0>        Histogram;
+		typedef StandardQuantiles<Histogram> Quantiles;
+
+		AccumulatorChainArray<
+				CoupledArrays<N, ValueType, LabelType>,
+				Select<
+						DataArg<1>,
+						LabelArg<2>,
+						Count,			// size
+						Sum,			// sum of intensity
+						Mean,			// mean intensity
+						Variance,		// variance of intensite
+						Skewness,		// ...
+						Kurtosis,		// ...
+						Histogram,		// histogram on intensities
+						Quantiles,      // 0, 10, 25, 50, 75, 90, 100
+						Coord<Variance>,// variance of cooridinates
+						Coord<Skewness>,// ...
+						Coord<Kurtosis>	// ...
+				>
+		> accumulator;
+
+		// set the histogram options
+		vigra::HistogramOptions histogramOptions;
+		histogramOptions = histogramOptions.setBinCount(20); 
+		histogramOptions = histogramOptions.setMinMax(0.0, 1.0); 
+		accumulator.setHistogramOptions(histogramOptions);
+
+		// don't compute features on the background
+		accumulator.ignoreLabel(0);
+
+		extractFeatures(image, labels, accumulator);
+
+		std::cout << "extracted features for " << accumulator.regionCount() << " regions" << std::endl;
+
+		for (LabelType i = 1;; i++) {
+
+			features[i].push_back(get<Count>(accumulator, i));
+			features[i].push_back(get<Sum>(accumulator, i));
+			features[i].push_back(get<Mean>(accumulator, i));
+			features[i].push_back(get<Variance>(accumulator, i));
+			features[i].push_back(get<Skewness>(accumulator, i));
+			features[i].push_back(get<Kurtosis>(accumulator, i));
+
+			for (unsigned int j = 0; j < 20; j++)
+				features[i].push_back(get<Histogram>(accumulator, i)[j]);
+			for (unsigned int j = 0; j < 7; j++)
+				features[i].push_back(get<Quantiles>(accumulator, i)[j]);
+
+			for (unsigned int d = 0; d < N; d++)
+				features[i].push_back(get<Coord<Variance> >(accumulator, i)[d]);
+			for (unsigned int d = 0; d < N; d++)
+				features[i].push_back(get<Coord<Skewness> >(accumulator, i)[d]);
+			for (unsigned int d = 0; d < N; d++)
+				features[i].push_back(get<Coord<Kurtosis> >(accumulator, i)[d]);
+
+			if (i == accumulator.maxRegionLabel())
+				break;
+		}
+	}
+};
+
+} // namespace region_features
+
+#endif // REGION_FEATURES_STATISTICS_H__
+
