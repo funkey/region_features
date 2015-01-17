39cf0d8f8e25f87f4116f3a9699b513519700c98
Jan Funke <funke@ini.ch>
2015-01-17 15:53:14 +0100

    implementation of ExternalFeatures, intermediate commit

--- ExternalFeatures.h
+++ ExternalFeatures.h
@@ -14,19 +14,44 @@ class ExternalFeatures {
 
 public:
 
+	ExternalFeatures(const std::string& filename) {
+
+		readFeaturesFile();
+	}
+
+	struct DefaultLabelLineMap;
+
+	/**
+	 * Append features for the given images and regions (provided via a label 
+	 * image) to a FeatureMap.
+	 *
+	 * @param image
+	 *              The raw image.
+	 *
+	 * @param labels
+	 *              A label image that defines the regions.
+	 *
+	 * @param features
+	 *              The feature map to append to.
+	 *
+	 * @param labelLineMap
+	 *              A functor that implements operator[] to map from LabelType 
+	 *              to a line number. If not given, the label number will be 
+	 *              used as the line number.
+	 */
+	template <typename LabelLineMap = DefaultLabelLineMap>
 	static void fill(
 			const vigra::MultiArrayView<N, ValueType>& image,
 			const vigra::MultiArrayView<N, LabelType>& labels,
-			FeatureMap<LabelType>&                     features) {
-
-		readFeaturesFile();
+			FeatureMap<LabelType>&                     features,
+			const LabelLineMap&                        labelLineMap = DefaultLabelLineMap()) {
 
 		LabelType _, maxLabel;
 		labels.minmax(_, maxLabel);
 
 		for (LabelType i = 1;; i++) {
 
-			addFeatures(i);
+			addFeatures(i, labelLineMap);
 
 			if (i == maxLabel)
 				break;
@@ -35,10 +60,21 @@ public:
 
 private:
 
+	/**
+	 * Default LabelLineMap to use. Maps each label id to the corresponding line 
+	 * number.
+	 */
+	struct DefaultLabelLineMap {
+		std::size_t operator[](LabelType i) { return i; }
+	};
+
 	void readFeaturesFile();
 
 	// add the features for label i
-	void addFeatures(LabelType i) {
+	template <typename LabelLineMap>
+	void addFeatures(LabelType i, const LabelLineMap& labelLineMap) {
+
+		std::size_t line =
 
 		// add each feature
 		//features.append(i, f);
