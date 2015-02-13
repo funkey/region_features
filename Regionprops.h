db63bb8c1260f4d794241898524b06fd0897654b
Chong Zhang <czhang@bbijnens.ca.upf.edu>
2015-02-13 18:18:54 +0100

    Added pointiness

--- Regionprops.h
+++ Regionprops.h
@@ -3,6 +3,7 @@
 
 #include <boost/lexical_cast.hpp>
 #include <vigra/accumulator.hxx>
+#include <vigra/polygon.hxx>
 
 namespace region_features {
 
@@ -14,9 +15,13 @@ public:
 	struct Parameters {
 
 		Parameters() :
-			numHistogramBins(20) {}
+			numAnglePoints(50),
+			contourVecAsArcSegmentRatio(0.1),
+			numAngleHistBins(16) {}
 
-		unsigned int numHistogramBins;
+		int numAnglePoints;
+		double contourVecAsArcSegmentRatio;
+		const int numAngleHistBins;
 	};
 
 	Regionprops(const Parameters& parameters = Parameters()) :
@@ -33,12 +38,15 @@ public:
 
 		//typedef UserRangeHistogram<0>        Histogram;
 		//typedef StandardQuantiles<Histogram> Quantiles;
+		Polygon<>		   			regionContours;
+		typedef Polygon<>::Point	Point;
 
 		AccumulatorChainArray<
 				CoupledArrays<N, ValueType, LabelType>,
 				Select<
 						DataArg<1>,
 						LabelArg<2>,
+						RegionContour,
 						RegionCircularity,	// circularity of a 2D region
 						RegionEccentricity	// eccentricity of a 2D region
 				>
@@ -50,8 +58,54 @@ public:
 		extractFeatures(image, labels, accumulator);
 
 		for (LabelType i = 1;; i++) {
-
 			if (N == 2) {
+				
+				////////////////////////////////////
+				//The code assumes that the contour is sampled uniformly, in counter-clockwise direction, and is defined by a pair of vectors of equal length (x and y). resolution alters the number of neighboring points the code takes into account in computing the tangent direction at any given point.
+				////////////////////////////////////
+				
+				regionContours = get<RegionContour>(accumulator, i);
+				//CHECK WHETHER last point is the same as the first on, i.e. closed contour
+				//int numAnglePoints = 50;
+				double arcDist = 1.0/_parameters.numAnglePoints*_parameters.contourVecAsArcSegmentRatio;
+				double avgAngle = 0;
+				//const int numAngleHistBins = 16;
+				int angleHist[_parameters.numAngleHistBins] = {0};
+
+				//assumes counter clockwise order
+				for (int ithAnglePoint=0; ithAnglePoint<_parameters.numAnglePoints; ithAnglePoint++) {
+
+					double quantileCenter = (double)ithAnglePoint/_parameters.numAnglePoints;
+					double c=regionContours.arcLengthQuantile(quantileCenter);
+					Point centerPt = regionContours.interpolate((int)floor(c), c - floor(c));
+
+					double prev=regionContours.arcLengthQuantile((quantileCenter-arcDist)>0 ? quantileCenter-arcDist : 1-arcDist+quantileCenter);
+					Point prevPt = regionContours.interpolate((int)floor(prev), prev - floor(prev));
+
+					double next=regionContours.arcLengthQuantile((quantileCenter+arcDist)>1 ? quantileCenter+arcDist-1 : quantileCenter+arcDist);
+					Point nextPt = regionContours.interpolate((int)floor(next), next - floor(next));
+
+					Point prevVec = prevPt - centerPt;
+					Point nextVec = nextPt - centerPt;
+
+					double cos_angle = std::min(1.0, std::max(-1.0, (prevVec[0]*nextVec[0]+prevVec[1]*nextVec[1]) / (norm(prevVec)*norm(nextVec))));
+
+					double angle = acos( cos_angle );
+
+					int binID = (int)floor(angle/M_PI*_parameters.numAngleHistBins);
+
+					if (binID==_parameters.numAngleHistBins)
+						binID--;
+					angleHist[binID] += 1;
+					avgAngle += angle;
+
+				}
+
+				////////////////////////////////////
+
+				features.append(i, avgAngle);
+				for (unsigned int d = 0; d < _parameters.numAngleHistBins; d++)
+					features.append(i, angleHist[d]);
 				features.append(i, get<RegionCircularity>(accumulator, i));
 				features.append(i, get<RegionEccentricity>(accumulator, i));
 			}
@@ -64,11 +118,55 @@ public:
 	void getFeatureNames(std::vector<std::string> featureNames) {
 
         if (N == 2) {
+        	featureNames.push_back("2D region pointyness");
 		    featureNames.push_back("2D region circularity");
 		    featureNames.push_back("2D region eccentricity");
 		} 
 		        
 	}
+
+//	computeContourPointyness() {
+//		////////////////////////////////////
+//		//The code assumes that the contour is sampled uniformly, in counter-clockwise direction, and is defined by a pair of vectors of equal length (x and y). resolution alters the number of neighboring points the code takes into account in computing the tangent direction at any given point.
+//		////////////////////////////////////
+//
+//		regionContours = get<RegionContour>(accumulator, i);
+//		//CHECK WHETHER last point is the same as the first on, i.e. closed contour
+//		int numAnglePoints = 50;
+//		double arcDist = 1.0/numAnglePoints*0.1;
+//		double avgAngle = 0;
+//		const int numAngleHistBins = 16;
+//		int angleHist[numAngleHistBins] = {0};
+//
+//		//assumes counter clockwise order
+//		for (int ithAnglePoint=0; ithAnglePoint<numAnglePoints; ithAnglePoint++) {
+//
+//			double quantileCenter = (double)ithAnglePoint/numAnglePoints;
+//			double c=regionContours.arcLengthQuantile(quantileCenter);
+//			Point centerPt = regionContours.interpolate((int)floor(c), c - floor(c));
+//
+//			double prev=regionContours.arcLengthQuantile((quantileCenter-arcDist)>0 ? quantileCenter-arcDist : 1-arcDist+quantileCenter);
+//			Point prevPt = regionContours.interpolate((int)floor(prev), prev - floor(prev));
+//
+//			double next=regionContours.arcLengthQuantile((quantileCenter+arcDist)>1 ? quantileCenter+arcDist-1 : quantileCenter+arcDist);
+//			Point nextPt = regionContours.interpolate((int)floor(next), next - floor(next));
+//
+//			Point prevVec = prevPt - centerPt;
+//			Point nextVec = nextPt - centerPt;
+//
+//			double cos_angle = std::min(1.0, std::max(-1.0, (prevVec[0]*nextVec[0]+prevVec[1]*nextVec[1]) / (norm(prevVec)*norm(nextVec))));
+//
+//			double angle = acos( cos_angle );
+//
+//			int binID = (int)floor(angle/M_PI*numAngleHistBins);
+//
+//			if (binID==numAngleHistBins)
+//				binID--;
+//			angleHist[binID] += 1;
+//			avgAngle += angle;
+//
+//		}
+//	}
 
 private:
 
